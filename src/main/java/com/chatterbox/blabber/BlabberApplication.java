f0c8816e07ff35bc73d5f0608d43a8ca33331e9e
parthpmp <parthparmaronline@gmail.com>
2026-06-16 07:17:18 +0530

    spring boot setup

---
+++ src/main/java/com/chatterbox/blabber/BlabberApplication.java
@@ -0,0 +1,13 @@
+package com.chatterbox.blabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlabberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatterbox/blabber/BlabberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatterbox.blabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
